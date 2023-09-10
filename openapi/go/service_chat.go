3059be415f34e19fdd5f14e52eaddc5e67b834be
iwase22334 <iwase22334@gmail.com>
2023-09-10 13:13:08 +0900

    WIP chat app

--- openapi/go/service_chat.go
+++ openapi/go/service_chat.go
@@ -6,6 +6,59 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type MessageQuery struct {
+	Data string
+}
+
+type SimpleMessage struct {
+	Data string
+}
+
+type Client struct {
+	UserID    string
+	SendQueue chan string
+}
+
+type ChatAPP struct {
+	Clients      []*Client
+	RecvQueue    chan string
+	ConnQueue    chan *Client
+	DisconnQueue chan *Client
+}
+
+func NewChatAPP() *ChatAPP {
+	return &ChatAPP{
+		Clients:      []*Client{},
+		ConnQueue:    make(chan *Client),
+		DisconnQueue: make(chan *Client),
+	}
+}
+
+func (c *ChatAPP) Run() {
+	for {
+		select {
+		case client := <-c.ConnQueue:
+			c.Append(client)
+		case client := <-c.DisconnQueue:
+			c.Remove(client)
+
+		}
+	}
+}
+
+func (c *ChatAPP) Append(client *Client) {
+	c.Clients = append(c.Clients, client)
+}
+
+func (c *ChatAPP) Remove(client *Client) {
+	for i, existingClient := range c.Clients {
+		if existingClient == client {
+			c.Clients = append(c.Clients[:i], c.Clients[i+1:]...)
+			break
+		}
+	}
+}
+
 func ChatEcho(conn *websocket.Conn) {
 	for {
 		messageType, p, err := conn.ReadMessage()
