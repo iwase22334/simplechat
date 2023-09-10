bc3b5521faabb09942d793d6241b58c6b0674212
iwase22334 <iwase22334@gmail.com>
2023-09-10 23:11:58 +0900

    remove comment

--- openapi/go/api_auth.go
+++ openapi/go/api_auth.go
@@ -19,37 +19,3 @@ type AuthAPI struct {
 	// User to login
 	Login gin.HandlerFunc
 }
-
-//func LoginHandler(r UserAuthRepository) gin.HandlerFunc {
-//	fn := func(c *gin.Context) {
-//		var ua UserAuth
-//		if err := c.ShouldBindJSON(&ua); err == nil {
-//			fmt.Println("context body", ua)
-//
-//			if Authenticate(r, ua) {
-//				c.JSON(http.StatusOK, gin.H{
-//					"message": "Login successful",
-//				})
-//
-//			} else {
-//				c.JSON(http.StatusBadRequest, gin.H{
-//					"message": "failed to login",
-//				})
-//			}
-//
-//		} else {
-//			fmt.Println("failed to convert", err)
-//			c.JSON(http.StatusBadRequest, gin.H{
-//				"message": "failed to login",
-//			})
-//		}
-//	}
-//
-//	return fn
-//}
-//
-//func NewAuthAPI(r UserAuthRepository) AuthAPI {
-//	return AuthAPI{
-//		Login: LoginHandler(r),
-//	}
-//}
