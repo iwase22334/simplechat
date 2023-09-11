775f5583da463d73cc8acb045d9126bb6ef1f25b
iwase22334 <iwase22334@gmail.com>
2023-09-11 14:55:48 +0900

    improve usability

--- simplechat/components/ChatTextField.tsx
+++ simplechat/components/ChatTextField.tsx
@@ -8,22 +8,26 @@ interface ChatTextFieldProps {
 
 export default function ChatTextField({createSocketRef} :ChatTextFieldProps) {
   const [line, setLine] = useState("")
-  return <Box display="flex" sx={{ alignItems: 'center' }}>
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log(data.get("text-field"))
+
+    createSocketRef.current?.send(line)
+    setLine("")
+  }
+
+  return <Box component="form" onSubmit={handleSubmit} display="flex" sx={{ alignItems: 'center' }}>
     <TextField
       id="text-field"
       value={line}
-      multiline
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         setLine(e.target.value)
       }}
     />
 
-    <IconButton color="primary" aria-label="send"
-      onClick={() => {
-        createSocketRef.current?.send(line)
-        setLine("")
-      }}
-    >
+    <IconButton type="submit" color="primary" aria-label="send" >
       <Send />
     </IconButton>
   </Box>
