12e27db2d98ecb681e7c5198bdc594bcbe7454e2
iwase22334 <iwase22334@gmail.com>
2023-09-20 16:40:18 +0900

    reverse proxy

--- backend/go/cors_middleware.go
+++ backend/go/cors_middleware.go
@@ -8,7 +8,7 @@ type CORSMiddleware struct {
 
 func _CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:51181")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:51182")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
